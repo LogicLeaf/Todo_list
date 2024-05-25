e1b1010797a327d9a2b2f2113fc4d9570513604f
Ali Bashir <ali.bashir713@gmail.com>
2024-05-25 21:17:52 +0500

    new

---
+++ index.js
@@ -0,0 +1,36 @@
+import inquirer from 'inquirer';
+const tasks = [];
+let continueAdding = true;
+while (continueAdding) {
+    const responses = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'task',
+            message: 'Please enter a task to add to your todo list:'
+        },
+        {
+            type: 'confirm',
+            name: 'more',
+            message: 'Would you like to add another task?',
+            default: false
+        }
+    ]);
+    const { task, more } = responses;
+    console.log(responses);
+    continueAdding = more;
+    if (task.trim()) {
+        tasks.push(task);
+    }
+    else {
+        console.log('Please enter a valid task.');
+    }
+}
+if (tasks.length > 0) {
+    console.log('Here is your Todo List:');
+    tasks.forEach((task) => {
+        console.log(task);
+    });
+}
+else {
+    console.log('No tasks were added to the todo list.');
+}
